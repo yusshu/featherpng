9209782ba7587e494357594db9b22f6c1595e7d9
yusshu <yushu@unnamed.team>
2023-12-18 00:10:25 -0500

    build!: move to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    `java-library`
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
